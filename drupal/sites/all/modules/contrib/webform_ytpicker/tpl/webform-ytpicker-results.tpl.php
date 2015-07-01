847ae74c0358c5120597fa3a23638fe1bb42e0ef
Frederik Wouters <woutersf@gmail.com>
2015-07-01 11:25:33 +0200

    Added updated module
    Signed-off-by: Frederik Wouters <woutersf@gmail.com>

--- drupal/sites/all/modules/contrib/webform_ytpicker/tpl/webform-ytpicker-results.tpl.php
+++
@@ -1,6 +0,0 @@
-<div class="<?php print $classes; ?>">
-  <div class="webform-analysis-component-inner">
-    <h3><?php print check_plain($component['name']); ?></h3>
-    <?php print drupal_render_children($component_analysis); ?>
-  </div>
-</div>
